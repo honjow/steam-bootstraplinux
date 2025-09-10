63be1ddd697b237da917c3fed6043f284f268433
honjow <honjow311@gmail.com>
2025-09-10 21:58:48 +0800

    sync

--- sync.sh
+++ sync.sh
@@ -13,6 +13,8 @@ done
 src_dir=~/.local/share/Steam/
 dst_dir=./bootstraplinux_ubuntu12_32/
 
+# ssh $user_name@$host_name  "rm -rf $src_dir/ubuntu12_32/steam-runtime.old"
+
 host_name=${host_name:-mini.local}
 user_name=${user_name:-gamer}
 rsync -avz --progress --delete -e "ssh -p 22" $user_name@$host_name:$src_dir $dst_dir \
@@ -20,6 +22,10 @@ rsync -avz --progress --delete -e "ssh -p 22" $user_name@$host_name:$src_dir $ds
     --exclude "logs" \
     --exclude "appcache" \
     --exclude ".cef-enable-remote-debugging" \
+    --exclude "userdata" \
     ${dry_run:+--dry-run}
 
 rm -rf $dst_dir/{steamapps,logs,appcache}
+
+rm -rf $dst_dir/config/avatarcache
+rm -rf $dst_dir/config/loginusers.vdf
