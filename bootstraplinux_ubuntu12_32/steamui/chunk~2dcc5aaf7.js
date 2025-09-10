6626674ff027acc330e333cc74e7b278822459e4
honjow <honjow311@gmail.com>
2025-09-10 21:58:48 +0800

    sync

--- bootstraplinux_ubuntu12_32/steamui/chunk~2dcc5aaf7.js
+++ bootstraplinux_ubuntu12_32/steamui/chunk~2dcc5aaf7.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:750c4c1759e2a2b2e0cf7c51cc0c79a26e93ee7b4efed39321a790a1d1651a21
-size 12577866
+oid sha256:442ec6f3fdbdc7b270f15a2000fe199b8a4b857705229b086a259b4b1e86875b
+size 12611959
--- bootstraplinux_ubuntu12_32/steamui/localization/steamui_arabic-json.js
+++ bootstraplinux_ubuntu12_32/steamui/localization/steamui_arabic-json.js
@@ -1,1 +1,1 @@
-"use strict";(self.webpackChunksteamui=self.webpackChunksteamui||[]).push([[2889],{46564:e=>{e.exports={}}}]);
+"use strict";(self.webpackChunksteamui=self.webpackChunksteamui||[]).push([[2889],{46564:e=>{e.exports=JSON.parse('{"language":"arabic"}')}}]);
